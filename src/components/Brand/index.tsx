30c1e8453de9e93ee99b937aed5aaa911d937f2e
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-03-28 17:04:04 -0400

    fixed layout card brand

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -2,7 +2,7 @@ import { Card } from "./Card"
 
 export const Brand = () => {
   return (
-    <div className="w-screen bg-blue-900 flex flex-col md:flex-row md:justify-center items-center gap-12">
+    <div className="w-screen bg-blue-900 flex justify-center items-center gap-12 md:gap-20 px-4 ">
       <Card number={4} description="ANOS NO MERCADO" icon="./images/group.png"/>
       <Card number={200} description="Painéis Solares" icon="./images/placas.png"/>
       <Card number={300} description="Clientes Satisfeitos" icon="./images/customers.png"/>
