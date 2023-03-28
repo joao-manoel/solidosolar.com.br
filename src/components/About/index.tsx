d3b6d59b0fff90aedc46273e5e705c56ebc0d8f2
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-03-28 17:41:48 -0400

    initialized animate

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,31 +1,16 @@
 export const About = () => {
   return (
-    <div className="bg-white py-6 px-5 flex md:flex-row flex-col justify-center">
-      
-
-      <div className="md:w-1/3 w-full  gap-2 flex flex-col">
+    <div className="bg-white py-6 px-5 flex justify-center">
+      <header className="relative flex justify-center">
+        <h1 className="text-9xl outlineText relative">Sobre</h1>
         <h1 className="
-          text-5xl border-l-4 border-orange-400 px-2 font-inter font-thin text-orange-400">
+          absolute top-2/4
+          font-bold text-green-500 text-4xl 
+          animate__animated animate__fadeIn
+        ">
           Quem Somos
         </h1>
-        <h3 className='text-3xl font-roboto text-gray-400 font-extralight'>
-          Somos especialistas em gerar resultados
-        </h3>
-        <span className="text-md text-gray-700">
-          É uma empresa brasileira MATOGROSSENSE que investe em inovação e busca a melhor tecnologia de geração de energia elétrica por capacitação solar para sistemas conectados à rede, com a maior rentabilidade possível para diversos portes de clientes.
-          A Solido Solar foi fundada em 2019, como resultado da reunião de empresários com mais de 19 anos no mercado de telecomunicações com vários cases de sucesso à frente de suas organizações e que possuem em comum a visão focada em desenvolver e oferecer ao mercado soluções que agregam a inovação e a sustentabilidade.
-        </span>
-      </div>
-
-
-      <div className="flex justify-center md:w-1/2 w-full">
-
-        <div className="flex flex-col items-center gap-4 px-2 py-2">
-          
-        </div>
-        
-      </div>
-      
+      </header>      
     </div>
   )
 }
