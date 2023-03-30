a78cd34638255c8296d9e1b79ad453e3d59af4ef
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-03-30 15:02:32 -0400

    fixed nav css

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,8 +5,12 @@ interface ButtonProps {
 
 export const Button = ({ children }: ButtonProps) => {
   return (
-    <button className="bg-blue-900 hover:bg-blue-800 text-white 
-      shadow-md py-2 px-6 rounded md:ml-8 duration-500 flex items-center text-lg gap-2">
+    <button className="bg-green-500 hover:bg-green-400 text-white 
+      shadow-md py-2 px-6  md:ml-8 duration-500 flex items-center text-lg gap-2
+      rounded-tl-2xl rounded-br-2xl
+      hover:rounded-tl-none hover:rounded-br-none
+      hover:rounded-bl-2xl hover:rounded-tr-2xl
+      ">
       {children}
     </button>
   )
--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -2,13 +2,11 @@ import Link from "next/link";
 import { useState } from "react";
 
 import {
-  HiClipboardCheck,
-  HiHome,
+  HiClipboardCheck, HiFlag, HiHome,
   HiMenu,
   HiX
 } from "react-icons/hi";
 import { MdBuild } from "react-icons/md";
-import { TfiBlackboard } from "react-icons/tfi";
 
 import { Button } from "../Button";
 
@@ -23,7 +21,7 @@ export const Nav = () => {
     {
       name: 'Sobre Nós',
       url: '#about',
-      icon: TfiBlackboard
+      icon: HiFlag
     },
     {
       name: 'Serviços',
@@ -42,7 +40,7 @@ export const Nav = () => {
       <nav
         className={`
           md:flex md:items-center 
-          gap-4 py-5 md:py-0  absolute md:static
+          gap-2 py-5 md:py-0  absolute md:static
           bg-white md:z-auto z-[990]
           left-0 w-full md:w-auto md:pl-0 pl-9
           transition-all duration-500 ease-in
@@ -51,14 +49,19 @@ export const Nav = () => {
         `}
       > {/**ul */}
         {Links.map((link, index) => (
-          <div className="text-xl hover:text-green-400 
-            duration-500 md:border-b-[1px] md:border-transparent md:hover:border-blue-900
-            rounded-sm md:hover:shadow-sm py-2 px-1 md:my-0 my-2"
+          <div className="text-xl sm:text-sm hover:text-green-400 
+            rounded-sm md:hover:shadow-sm py-2 px-1 md:my-0 my-2
+            hover:bg-white hover:drop-shadow-md duration-500 md:border-b-[1px] 
+            md:border-transparent md:hover:border-green-400
+            "
             key={index}
           >
             <Link
               href={link.url}
-              className="flex items-center gap-1 text-gray-700 hover:text-gray-500"
+              className="
+                flex items-center gap-1 text-gray-900 hover:text-gray-500
+                px-2 py-2
+               "
             >
               {link.icon && <link.icon />}
               <span>{link.name}</span>
