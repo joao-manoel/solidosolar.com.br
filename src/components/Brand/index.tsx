0eca1d5c03b82675f03f3b1160d3a8b0f1e80b6c
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-03-30 14:42:30 -0400

    number

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -4,7 +4,7 @@ export const Brand = () => {
   return (
     <div className="w-full bg-blue-900 flex justify-center items-center gap-12 md:gap-20 px-4 ">
       <Card number={4} description="ANOS NO MERCADO" icon="./images/group.png"/>
-      <Card number={200} description="Painéis Solares" icon="./images/placas.png"/>
+      <Card number={5000} description="Painéis Solares" icon="./images/placas.png"/>
       <Card number={300} description="Clientes Satisfeitos" icon="./images/customers.png"/>
     </div>
   )
