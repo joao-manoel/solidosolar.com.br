e6703289f8f4724dc1a604870e617e0fe3f2f252
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-03-30 16:18:01 -0400

    fixed css button nav

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 export const Button = ({ children }: ButtonProps) => {
   return (
     <button className="
-      w-full lg:w-auto
+      w-11/12 lg:w-auto
     bg-green-500 hover:bg-green-400 text-white 
       shadow-md py-4 lg:py-2 px-6  md:ml-8 duration-500 flex items-center justify-center lg:justify-start
       text-lg gap-2 
