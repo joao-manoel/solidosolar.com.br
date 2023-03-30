76886cc4940d28e1e16b372841e77e982daa8c08
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-03-30 16:13:36 -0400

    fixed navbar items

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,8 +5,11 @@ interface ButtonProps {
 
 export const Button = ({ children }: ButtonProps) => {
   return (
-    <button className="bg-green-500 hover:bg-green-400 text-white 
-      shadow-md py-2 px-6  md:ml-8 duration-500 flex items-center text-lg gap-2
+    <button className="
+      w-full lg:w-auto
+    bg-green-500 hover:bg-green-400 text-white 
+      shadow-md py-4 lg:py-2 px-6  md:ml-8 duration-500 flex items-center justify-center lg:justify-start
+      text-lg gap-2 
       rounded-tl-2xl rounded-br-2xl
       hover:rounded-tl-none hover:rounded-br-none
       hover:rounded-bl-2xl hover:rounded-tr-2xl
